2b7c48d9c0459f49adac707b79263c4ee4f87f0f
luciendberkeley <lucienduchateau@students.berkeley.net>
2024-08-31 01:42:05 +0000

    Good simplification, can subtract, add, divide, and multiply out equations. YES

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const addLine = () => {
 }
 
 addLine();
-inputs[0].value = "2*x+3=7";
+inputs[0].value = "2*a+3=7";
 
 document.addEventListener("keypress", (event) => {
 	if(event.key == "Enter" && event.shiftKey) {
@@ -92,35 +92,35 @@ class ASTNode {
 	}
 }
 
-const lookForOp = (operation, array) => {
+const lookForOps = (operations, array) => {
 	let left = [];
 	let right = [];
 	let operation_found = false;
 	for(let i=array.length - 1; i >= 0; i--) {
-		if(array[i] == operation && operation_found == false) {
-			operation_found = true;
+		if(operations.includes(array[i]) && operation_found == false) {
+			operation_found = i;
 		} else {
 			(operation_found ? left : right).push(array[i]);
 		}
 	}
 
-	if(operation_found == true) {
-		return {"found": true, "node": new ASTNode("operation", operation, left, right)};
-	} else {
+	if(operation_found === false) {
 		return {"found": false, "node": array}
+	} else {
+		return {"found": true, "node": new ASTNode("operation", array[operation_found], left, right)};
 	}
 };
 
 const checkAllOps = (array) => {
-	let processed = lookForOp("=", array);
+	let processed = lookForOps(["="], array);
 	let found = processed.found;
 	let node = processed.node;
 	if(found == false) {
-		processed = lookForOp("+", array);
+		processed = lookForOps(["+", "-"], array);
 		found = processed.found;
 		node = processed.node;
 		if(found == false) {
-			processed = lookForOp("*", array);
+			processed = lookForOps(["*", "/"], array);
 			found = processed.found;
 			node = processed.node;
 			if(found == false) {
@@ -136,7 +136,6 @@ const checkAllOps = (array) => {
 			return new ASTNode("operation", node.value, node.left, node.right);
 		}
 	} else {
-		// console.log(`Operation found: ${node.value}, 1`);
 		return new ASTNode("operation", node.value, node.left, node.right);
 	}
 };
@@ -150,21 +149,97 @@ const processTokens = (tokens) => {
 	}
 };
 
-const simplifyAST = (AST) => {
+const simplifyAST1 = (AST) => {
 	let new_ast = AST;
 
 	if(AST.type == "operation") {
-		if(AST.value == "+") {
-			if(AST.left.type == "operand_n" && AST.right.type == "operand_n") {
-				new_ast = new ASTNode("operand_n", AST.left.value + AST.right.value);
+		if(AST.value == "+" || AST.value == "-") {
+			new_ast = new ASTNode("operation", AST.value, simplifyAST1(AST.left), simplifyAST1(AST.right));
+
+			if(new_ast.left.type == "operand_n" && new_ast.right.type == "operand_n") {
+				new_ast = new ASTNode("operand_n", eval(`${new_ast.left.value} ${new_ast.value} ${new_ast.right.value}`));
+			} else if(new_ast.left.type == "operand_n") {
+				if(new_ast.right.value == "+" || new_ast.right.value == "-") {
+					if(new_ast.right.left.type == "operand_n") {
+						new_ast = new ASTNode("operation", new_ast.value, new ASTNode("operand_n", eval(`${new_ast.left.value} ${new_ast.value} ${new_ast.right.left.value}`)), new_ast.right.right);
+					} else if(new_ast.right.right.type == "operand_n") {
+						new_ast = new ASTNode("operation", new_ast.value, new ASTNode("operand_n", eval(`${new_ast.left.value} ${new_ast.value} ${new_ast.right.right.value}`)), new_ast.right.left);
+					}
+				}
+			} else if(new_ast.right.type == "operand_n") {
+				if(new_ast.left.value == "+" || new_ast.left.value == "-") {
+					if(new_ast.left.left.type == "operand_n") {
+						new_ast = new ASTNode("operation", new_ast.value, new ASTNode("operand_n", eval(`${new_ast.right.value} ${new_ast.value} ${new_ast.left.left.value}`)), new_ast.left.right);
+					} else if(new_ast.left.right.type == "operand_n") {
+						new_ast = new ASTNode("operation", new_ast.value, new ASTNode("operand_n", eval(`${new_ast.right.value} ${new_ast.value} ${new_ast.left.right.value}`)), new_ast.left.left);
+					}
+				}
+
 			}
-		} else if(AST.value == "*") {
-			if(AST.left.type == "operand_n" && AST.right.type == "operand_n") {
-				new_ast = new ASTNode("operand_n", AST.left.value * AST.right.value);
+		} else if(AST.value == "*" || AST.value == "/") {
+			new_ast = new ASTNode("operation", AST.value, simplifyAST1(AST.left), simplifyAST1(AST.right));
+			
+			let left_valid = new_ast.left.type == "operand_n";
+			let right_valid = new_ast.right.type == "operand_n";
+
+			if(left_valid == true && right_valid == true) {
+				new_ast = new ASTNode("operand_n", eval(`${new_ast.left.value} ${new_ast.value} ${new_ast.right.value}`));
 			}
 		} else if(AST.value == "=") {
-			new_ast = new ASTNode("operation", "=", simplifyAST(AST.left), simplifyAST(AST.right));
+			new_ast = new ASTNode("operation", "=", simplifyAST1(AST.left), simplifyAST1(AST.right));
 		}
+	}
+
+	return new_ast
+};
+
+const simplifyAST2 = (AST, count=0) => {
+	let new_ast = AST;
+
+	if(count > 30) {
+		return new_ast
+	}
+
+	if(new_ast.value == "=") {
+		if(new_ast.left.value == "+") {
+			if(new_ast.left.left.type == "operand_n") {
+				new_ast.right.value -= new_ast.left.left.value;
+				new_ast.left = new_ast.left.right;
+			} else if(new_ast.left.right.type == "operand_n") {
+				new_ast.right.value -= new_ast.left.right.value;
+				new_ast.left = new_ast.left.left;
+			}
+		} else if(new_ast.left.value == "-") {
+			if(new_ast.left.left.type == "operand_n") {
+				new_ast.right.value += new_ast.left.left.value;
+				new_ast.left = new_ast.left.right;
+			} else if(new_ast.left.right.type == "operand_n") {
+				new_ast.right.value += new_ast.left.right.value;
+				new_ast.left = new_ast.left.left;
+			}
+		} else if(new_ast.left.value == "*") {
+			if(new_ast.left.left.type == "operand_n") {
+				new_ast.right.value /= new_ast.left.left.value;
+				new_ast.left = new_ast.left.right;
+			} else if(new_ast.left.right.type == "operand_n") {
+				new_ast.right.value /= new_ast.left.right.value;
+				new_ast.left = new_ast.left.left;
+			}
+		} else if(new_ast.left.value == "/") {
+			if(new_ast.left.left.type == "operand_n") {
+				new_ast.right.value *= new_ast.left.left.value;
+				new_ast.left = new_ast.left.right;
+			} else if(new_ast.left.right.type == "operand_n") {
+				new_ast.right.value *= new_ast.left.right.value;
+				new_ast.left = new_ast.left.left;
+			}
+		}
+		if(new_ast.left.type != "operand_v") {
+			console.log(new_ast)
+			new_ast = simplifyAST2(new_ast, count + 1);
+		}
+	} else {
+		console.error("Invalid Equation.")
 	}
 
 	return new_ast
@@ -172,10 +247,11 @@ const simplifyAST = (AST) => {
 
 const generateAST = (tokens) => {
 	let starting_ast = processTokens(tokens);
-	let simplified = simplifyAST(starting_ast);
+	let simplified = simplifyAST1(starting_ast);
+	simplified = simplifyAST2(simplified);
+	console.log(`Before: ${JSON.stringify(starting_ast)}, After: ${JSON.stringify(simplified)}`)
 
 	let equation = returnEquation(simplified);
-	console.log(equation);
 
 	return equation
 };
@@ -195,15 +271,14 @@ const returnEquation = (AST) => {
 const change = (values) => {
 	for(let i=0; i < values.length; i++) {
 		message = values[i];
-		if(message != "") {
-			outputs[i].innerText = `$${message}$`;
+		let tokens = generateTokens(message);
+		let ast = generateAST(tokens);
+		
+		if(ast !== "") {
+			outputs[i].innerText = `$${ast}$`;
 		} else {
 			outputs[i].innerText = "";
 		}
-
-
-		let tokens = generateTokens(values[i]);
-		let ast = generateAST(tokens);
 	}
 }
 
